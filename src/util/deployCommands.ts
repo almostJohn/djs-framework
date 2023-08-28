73a843786d874d5d4986ed48a21c89ba961a8add
Jhan Gale Garcia <garcia.johngale@gmail.com>
2023-08-28 17:41:37 +0800

    fix(bug): change parameter to isGlobal instead of isGuildRegistered

--- src/util/deployCommands.ts
+++ src/util/deployCommands.ts
@@ -2,32 +2,28 @@ import process from "node:process";
 import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v10";
 
-export function loadCommands<T extends []>(commands: T): T {
-	return commands;
-}
-
-export async function deployCommands(rest: REST, loadCommands: [], isGuildRegister = true) {
-	if (!isGuildRegister) {
+export async function deployCommands(rest: REST, loadCommands: [], isGlobal = true) {
+	if (!isGlobal) {
 		try {
-			console.log("Started refresing guilded interaction (/) commands");
+			console.log("Guilded interaction (/) commands started");
 
 			await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID!, process.env.GUILD_ID!), {
 				body: loadCommands,
 			});
 
-			console.log("Successfully reloaded guilded interaction (/) commands");
+			console.log("Guilded interaction (/) commands reloaded");
 		} catch (error) {
 			console.error(error);
 		}
 	} else {
 		try {
-			console.log("Started refreshing globally interaction (/) commands");
+			console.log("Global interaction (/) commands started");
 
 			await rest.put(Routes.applicationCommands(process.env.CLIENT_ID!), {
 				body: loadCommands,
 			});
 
-			console.log("Successfully reloaded globally interaction (/) commands");
+			console.log("Global interaction (/) commands reloaded");
 		} catch (error) {
 			console.error(error);
 		}
