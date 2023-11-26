fff776d416ba544eb0968cf738e9931c07ce0d0b
Jhan Gale Garcia <garcia.johngale@gmail.com>
2023-11-26 18:45:26 +0800

    update(framework): did logger options parameter optional

--- src/logger.ts
+++ src/logger.ts
@@ -9,16 +9,16 @@ type LoggerOptions = {
 export const pinoLogger = pino({ name: process.env.LOGGER_NAME! });
 
 export const logger = {
-	error(options: LoggerOptions, ...args: unknown[]) {
+	error(options?: LoggerOptions, ...args: unknown[]) {
 		console.log(chalk.red(...args), options);
 	},
-	warn(options: LoggerOptions, ...args: unknown[]) {
+	warn(options?: LoggerOptions, ...args: unknown[]) {
 		console.log(chalk.yellow(...args), options);
 	},
-	info(options: LoggerOptions, ...args: unknown[]) {
+	info(options?: LoggerOptions, ...args: unknown[]) {
 		console.log(chalk.cyan(...args), options);
 	},
-	success(options: LoggerOptions, ...args: unknown[]) {
+	success(options?: LoggerOptions, ...args: unknown[]) {
 		console.log(chalk.green(...args), options);
 	},
 };
