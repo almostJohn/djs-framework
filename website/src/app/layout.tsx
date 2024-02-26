6dbc4eecedbb8fd080242d151e17ae779dd585a9
Jhan Gale Garcia <garcia.johngale@gmail.com>
2024-02-26 17:49:06 +0800

    feat: website icon
    * refactor: chore deps

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -6,23 +6,37 @@ import { Providers } from "./providers";
 import "../styles/custom.css";
 import "../styles/globals.css";
 
-export const viewport: Viewport = {
+export const viewport = {
 	themeColor: [
 		{ media: "(prefers-color-scheme: light)", color: "#f5f5f5" },
 		{ media: "(prefers-color-scheme: dark)", color: "#171717" },
 	],
 	colorScheme: "light dark",
-};
+} satisfies Viewport;
 
-export const metadata: Metadata = {
+export const metadata = {
 	title: {
 		default: "djs-framework",
 		template: "% | djs-framework",
 	},
+	icons: {
+		other: [
+			{
+				url: "/djs-framework.png",
+				sizes: "32x32",
+				type: "image/png",
+			},
+			{
+				url: "/djs-framework.png",
+				sizes: "16x16",
+				type: "image/png",
+			},
+		],
+	},
 	other: {
 		"msapplication-TileColor": "#171717",
 	},
-};
+} satisfies Metadata;
 
 export default function RootLayout({ children }: PropsWithChildren) {
 	return (
