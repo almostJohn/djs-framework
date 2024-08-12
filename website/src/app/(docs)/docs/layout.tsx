aec6b3e9a31649ff37c3abe66776b3a94ec077f3
almostJohn <garcia.johngale@gmail.com>
2024-08-12 16:40:08 +0800

    fix: docs layout

--- website/src/app/(docs)/docs/layout.tsx
+++ website/src/app/(docs)/docs/layout.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { DocsNavbar } from "~/components/docs/DocsNavbar";
 
-export default function DocsLayout({ children }: { readonly children: React.PropsWithChildren }) {
+export default function DocsLayout({ children }: { readonly children: React.ReactNode }) {
 	return (
 		<>
 			<DocsNavbar />
